6121a58318f626d293aaac2cc34901cf7f0d17e8
Hrishikesh Baidya <hrishikeshbaidya7@gmail.com>
2018-03-22 15:59:50 +0530

    Adding portfolio page

--- server.js
+++ server.js
@@ -52,6 +52,12 @@ app.get('/about', (req, res) =>{
   });
 });
 
+app.get('/portfolio', (req, res) =>{
+  res.render('portfolio.hbs',{
+    pageTitle:'Portfolio Page',
+  });
+});
+
 // /bad --send back json with erromessage
 app.get('/bad', (req, res) => {
   res.send({
